46e44328237674cb7b2210ee4a560f83500c49f0
nyxnmigi5 <a64569773@gmail.com>
2024-12-17 04:55:14 +0900

    Update kakaoMap.js

--- kakaoMap.js
+++ kakaoMap.js
@@ -32,31 +32,31 @@ if (navigator.geolocation) {
     displayMarker(locPosition, message);
 }
 
-// 지도에 마커와 인포윈도우를 표시하는 함수입니다
-    var marker;
-    var flag = false;
-function displayMarker(locPosition, message) {
-    if(flag){
-        marker.setMap(null);
-    }
-    // 마커를 생성합니다
-    var marker = new kakao.maps.Marker({
-        map: map,
-        position: locPosition
-    });
-//     marker.setMap(map);
-    flag = true;
-    var iwContent = message, // 인포윈도우에 표시할 내용
-        iwRemoveable = true;
+    // 지도를 클릭한 위치에 표출할 마커입니다
+var marker = new kakao.maps.Marker({ 
+    // 지도 중심좌표에 마커를 생성합니다 
+    position: map.getCenter() 
+}); 
+// 지도에 마커를 표시합니다
+marker.setMap(map);
 
-    // 인포윈도우를 생성합니다
-    var infowindow = new kakao.maps.InfoWindow({
-        content : iwContent,
-        removable : iwRemoveable
-    });
-
-    // 인포윈도우를 마커위에 표시합니다 
-    infowindow.open(map, marker);
+// 지도에 클릭 이벤트를 등록합니다
+// 지도를 클릭하면 마지막 파라미터로 넘어온 함수를 호출합니다
+kakao.maps.event.addListener(map, 'click', function(mouseEvent) {        
+    
+    // 클릭한 위도, 경도 정보를 가져옵니다 
+    var latlng = mouseEvent.latLng; 
+    
+    // 마커 위치를 클릭한 위치로 옮깁니다
+    marker.setPosition(latlng);
+    
+    var message = '클릭한 위치의 위도는 ' + latlng.getLat() + ' 이고, ';
+    message += '경도는 ' + latlng.getLng() + ' 입니다';
+    
+    var resultDiv = document.getElementById('clickLatlng'); 
+    resultDiv.innerHTML = message;
+    
+});
     
     // 지도 중심좌표를 접속위치로 변경합니다
     map.setCenter(locPosition);      
